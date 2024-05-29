3a93e094877f5ada408e78c51f9f0625f6484bcd
e_arandia <ewarandia@gmail.com>
2024-05-29 08:42:36 -0400

    clase sesion 13

---
+++ retos_sesion_04/ejercicio_01.py
@@ -0,0 +1,8 @@
+# Este ejercicio dibuja un cuadrado con varios prints
+print("****************")
+print("*              *")
+print("*              *")
+print("*              *")
+print("*              *")
+print("*              *")
+print("****************")
