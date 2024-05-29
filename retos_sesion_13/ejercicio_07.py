f1ef672eabec283e5f12b234ca715eabe2b86b15
e_arandia <ewarandia@gmail.com>
2024-05-29 16:22:20 -0400

    retos_sesion_12 ipynb

---
+++ retos_sesion_13/ejercicio_07.py
@@ -0,0 +1,7 @@
+# Crear una serie de números del 1 al 100, 
+# si el número es divisible por 3 imprimir Fizz, 
+# si el número es divisible por 5 imprimir Buzz, 
+# si el número es divisible por 3 y 5 imprimir FizzBuzz
+
+
+# Convertir y ejecutar el archivo de la "sesion13.py" a un archivo en jupyter notebook
