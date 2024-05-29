069d3507324c3cffdafd41b4946eac22f593c763
e_arandia <ewarandia@gmail.com>
2024-05-29 08:59:15 -0400

    clase sesion 05

--- sesion05.py
+++ sesion05.py
@@ -72,31 +72,31 @@ print(variable_6)
 print ( type (variable_6) )
 
 # Operadores aritméticos
-	a = 10
-	b = 3
-	# Suma
-	print ("Suma")
-	print (a + b)
-	# Resta
-	print ("Resta")
-	print (a - b)
-	# Multiplicación
-	print ("Multiplicación")
-	print (a * b)
-	# División
-	print ("División")
-	print (a / b)
-	# Potencia
-	print ("Potencia")
-	print (a ** b)
-	# Módulo o residuo
-	print ("Módulo o residuo")
-	print (a % b)
-	# División entera
-	print ("División entera")
-	print (a // b)
+a = 10
+b = 3
+# Suma
+print ("Suma")
+print (a + b)
+# Resta
+print ("Resta")
+print (a - b)
+# Multiplicación
+print ("Multiplicación")
+print (a * b)
+# División
+print ("División")
+print (a / b)
+# Potencia
+print ("Potencia")
+print (a ** b)
+# Módulo o residuo
+print ("Módulo o residuo")
+print (a % b)
+# División entera
+print ("División entera")
+print (a // b)
 
-    # Operaciones más complejas
+# Operaciones más complejas
 minutos = 300
 tiempo_extra_segundos = 3600
 horas = (minutos + tiempo_extra_segundos / 60) / 60
