6be15ec413ea52f3a1e480faa806dab19083e852
e_arandia <ewarandia@gmail.com>
2024-05-29 09:01:55 -0400

    retos_sesion_04 py

---
+++ retos_sesion_04/ejercicio_03.py
@@ -0,0 +1,12 @@
+# Crea un archivo "ejercicio_03.py", 
+# escribe las reglas de tu juego de mesa favorito con múltiples prints, por ejemplo:
+print("Reglas juego damas")
+print("1.Se juega en un tablero 8x8 con casillas negras y blancas")
+print("2.Las fichas se colocan solo en las casillas negras")
+print("3.EL movimiento es una casilla a la vez y solo se pueden mover diagonalmente ")
+print("4.Para capturar la ficha del jugador contrario debe exitir una casilla vacia en la parte de atras de la ficha del adversario")
+print("5.Si una ficha llega al lado opuesto del cual comenzó el juego se puede mover por el tablero la cantidad de casillas que desee")
+print("6.Pero siempre en forma diagonal")
+print("7.El juego finaliza cuando: ")
+print("8.a) Se logran capturar todas las fichas del ponente")
+print("9.b) Se logra inmovilizar las fichas")
