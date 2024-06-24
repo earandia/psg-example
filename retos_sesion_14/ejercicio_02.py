1a6c3846c4b257496dfdcc61c27253825914c03b
e_arandia <ewarandia@gmail.com>
2024-06-24 16:35:12 -0400

    retos sesion 14

--- retos_sesion_14/ejercicio_02.py
+++ retos_sesion_14/ejercicio_02.py
@@ -2,3 +2,24 @@
 # como argumentos de palabras clave.
 # realice los cálculos correspondientes, 
 # las operaciones son suma, resta, multiplicación y división
+
+def datos_persona(**datos):
+    resultado = ""
+    if datos['op'] == 'suma':
+        resultado = f"{datos['numero_1']} + {datos['numero_2']} = {datos['numero_1'] + datos['numero_2']}"
+    elif datos['op'] == 'resta':
+        resultado = f"{datos['numero_1']} - {datos['numero_2']} = {datos['numero_1'] - datos['numero_2']}"
+    elif datos['op'] == 'multiplicacion':
+        resultado = f"{datos['numero_1']} * {datos['numero_2']} = {datos['numero_1'] * datos['numero_2']}"
+    elif datos['op'] == 'division':
+        resultado = f"{datos['numero_1']} / {datos['numero_2']} = {datos['numero_1'] / datos['numero_2']}"
+    else:
+        resultado = "Operacion invalida"
+    return resultado   
+
+
+numero_1 = int(input("Ingrese un numero: "))
+numero_2 = int(input("Ingrese otro numero: "))
+operador = input("Ingrese operacion: ")
+resultado = datos_persona(op = operador, numero_1 = numero_1, numero_2 = numero_2)
+print (resultado)
--- retos_sesion_14/ejercicio_07.py
+++ retos_sesion_14/ejercicio_07.py
@@ -1,5 +1,16 @@
 # Simular un tres en raya con funciones donde reciba las jugadas 
 # y devuelva el ganador hasta que alguien ingrese salir
 
+def jugar_tres_en_raya(turno1, turno2):
+    pass
 
-# Convertir y ejecutar el archivo de la "sesion14.py" a un archivo en jupyter notebook
+print("Tres en raya")
+while True:
+    jugador1 = input("Posición Jugador 1: ")
+    if jugador1 == "salir":
+        break
+    jugador2 = input("Posición Jugador 2: ")
+    if jugador2 == "salir":
+        break
+    resultado = jugar_tres_en_raya(jugador1, jugador2)
+    print (resultado)
